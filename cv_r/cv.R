48386bbb90717c4b9ea799c4bcac270b9dff4494
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2025-07-21 05:52:16 +0700

    cv (goeveg) With (In) R Software

---
+++ cv_r/cv.R
@@ -0,0 +1,14 @@
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Coefficient of variation (CV) Use cv (goeveg) With (In) R Software
+install.packages("goeveg")
+library("goeveg")
+cv = read.csv("https://raw.githubusercontent.com/timbulwidodostp/cv_r/main/cv_r/cv.csv",sep = ";")
+# Estimation Coefficient of variation (CV) Use cv (goeveg) With (In) R Software
+cv(cv$cv)
+# Coefficient of variation (CV) Use cv (goeveg) With (In) R Software
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Finished
